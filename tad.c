7c1d954363a0344f6356872e7f4db7ab751dd09c
GlaiconFF <99925470+GlaiconFF@users.noreply.github.com>
2022-08-09 13:57:00 -0300

    Add files via upload

---
+++ tad.c
@@ -0,0 +1,10 @@
+#include "float_vector.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int agc, char const *argv[]){
+    FloatVector *vetor = create(10);
+
+    return 0;
+}
