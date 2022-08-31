830d8a1c7bdd3625a40c28a1b3f9b3071b7fd0e9
GlaiconFF <99925470+GlaiconFF@users.noreply.github.com>
2022-08-30 21:40:58 -0400

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define T 100
+#include <time.h>
+#define T 50000
+
+int main ()
+{
+    double tempo = 0.0;
+    clock_t inicio = clock();
+
+    int vetor[T], troca = 0, var, comp;
+    
+    for (int i = 0; i < T; i++)
+    {
+        vetor[i] = rand() %100;
+    }
+    
+    quicksort(vetor, 0, T);
+
+    clock_t final = clock();
+    tempo += (double)(final - inicio) / CLOCKS_PER_SEC;
+    printf("O tempo decorrido foi de %f segundos", tempo);
+
+    return 0;
+}
 
 void swap(int *a, int *b)
 {
@@ -9,18 +31,41 @@ void swap(int *a, int *b)
     *b = aux;
 }
 
-void quicksort(int *vet, int first, int final)
+void quicksort(int *vetor, int inicio, int final)
 {
     int pi;
-    if(final > first)
+    if(final > inicio)
     {
-        pi = part(vet, first, final);
-        quicksort(vet, first, pi - 1);
-        quicksort(vet, pi + 1, final);
+        pi = part(vetor, inicio, final);
+        quicksort(vetor, inicio, pi - 1);
+        quicksort(vetor, pi + 1, final);
     }    
 }
 
-// adicionar ultima parte
+int part(int *vetor, int inicio, int final)
+{
+    int esq = inicio, dir = final, pi = vetor[inicio];
+
+    while(dir > esq)
+    {
+        while(vetor[esq] <= pi && esq <= final)
+        {
+            esq++;
+        }
+        while(vetor[dir] > pi && dir >= inicio)
+        {
+            dir--;
+        }
+
+        if(esq < dir)
+        {
+            swap(&vetor[dir], &vetor[esq]);
+        }
+    }
+    vetor[inicio] = vetor[dir];
+    vetor[dir] = pi;
+    return dir;
+}
 
 void printVector(const int *vetor)
 {
@@ -28,25 +73,4 @@ void printVector(const int *vetor)
     {
         printf("Vetor[%d] = %d\n", i, vetor[i]);
     }
-
-    for(int i = 0; i < T; i++)
-    {
-        printf("Vetor[%d] = %d\n", i, vetor[i]);
-    }
-}
-
-int main ()
-{
-
-    int vet[T];
-    
-    for (int i = 0; i < T; i++)
-    {
-        vet[i] = rand() %100;
-    }
-    
-    quicksort(vet, 0, T);
-    printVector(&vet);
-
-    return 0;
 }
